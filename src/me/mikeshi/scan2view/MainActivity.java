6eb36d13562c22d7bda6a5d60c25f4c70358c6fd
mikeshi80 <mikeshi80@gmail.com>
2012-11-25 22:49:54 +0800

    adjust the layout

--- src/me/mikeshi/scan2view/MainActivity.java
+++ src/me/mikeshi/scan2view/MainActivity.java
@@ -1,20 +1,33 @@
 package me.mikeshi.scan2view;
 
+import java.io.File;
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView mDir;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkIfDirSpecified();
     }
 
-    @Override
+    private void checkIfDirSpecified() {
+    	
+    	File external = Environment.getExternalStorageDirectory();
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    
 }

27bd8108617c0947c7cf3fd1c19f7ed167fcf178
Mike Shi <mikeshi80@gmail.com>
2012-11-26 17:42:09 +0800

    adjust the UI
    add the basic support for bar scan.

--- gen/me/mikeshi/scan2view/R.java
+++ gen/me/mikeshi/scan2view/R.java
@@ -11,12 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int bg_manual_enter=0x7f020000;
+        public static final int bg_scan=0x7f020001;
+        public static final int bg_zebar_barcode=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070002;
-        public static final int scan_barcode=0x7f070001;
+        public static final int enter_barcode=0x7f070001;
+        public static final int history=0x7f070003;
+        public static final int menu_settings=0x7f070004;
+        public static final int scan_barcode=0x7f070002;
         public static final int selected_folder=0x7f070000;
     }
     public static final class layout {
--- src/me/mikeshi/scan2view/MainActivity.java
+++ src/me/mikeshi/scan2view/MainActivity.java
@@ -2,20 +2,29 @@ package me.mikeshi.scan2view;
 
 import java.io.File;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
 	private TextView mDir;
+	private TextView mScan;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkIfDirSpecified();
+        
+        mScan = (TextView) findViewById(R.id.scan_barcode);
+		mScan.setOnClickListener(this);
     }
 
     private void checkIfDirSpecified() {
@@ -28,6 +37,25 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	@Override
+	public void onClick(View v) {
+		if (v == mScan) {
+			IntentIntegrator integrator = new IntentIntegrator(this);
+			integrator.initiateScan();
+		}
+		
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		  IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+		  if (scanResult != null) {
+		    // handle scan result
+		  }
+		  // else continue with any other code you need in the method
+	}
     
+	
     
 }
